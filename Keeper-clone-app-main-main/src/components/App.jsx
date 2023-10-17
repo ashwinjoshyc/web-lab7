d65d3a48e58cdd76eb25dbf2144431f6b53e39d8
ashwinjoshyc <141505212+ashwinjoshyc@users.noreply.github.com>
2023-10-17 12:10:58 +0530

    Add files via upload

---
+++ Keeper-clone-app-main-main/src/components/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "../css/styles.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <CreateArea onAdd={addNote} />
+        <section className="notes-container">
+          {notes.map((noteItem, index) => {
+            return (
+              <Note
+                key={index}
+                id={index}
+                title={noteItem.title}
+                content={noteItem.content}
+                onDelete={deleteNote}
+              />
+            );
+          })}
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
